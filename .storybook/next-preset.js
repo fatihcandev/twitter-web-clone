1db1f5e50d604bf9e2cecaa8cb5eb18f1c6d33b3
Fatih <fatihcan.social@gmail.com>
2020-06-09 20:47:28 +0300

    build fix

--- .storybook/next-preset.js
+++ .storybook/next-preset.js
@@ -41,7 +41,7 @@ module.exports = {
           options: {
             sourceMap: true,
             config: {
-              path: './'
+              path: './.storybook/'
             }
           }
         }
