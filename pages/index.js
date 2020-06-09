913da73c6e22403713e59cb4977697ceb5be13ea
Fatih <fatihcan.social@gmail.com>
2020-06-09 15:57:51 +0300

    initial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Button from '../.storybook/components/button'
+
+const Home = () => (
+  <div>
+    <h1>Welcome to Next.js!</h1>
+    <Button>Merhaba</Button>
+  </div>
+)
+
+export default Home
