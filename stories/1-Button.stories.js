b204d795de5358b8f16494118467a8252db34117
Fatih <fatihcan.social@gmail.com>
2020-06-11 21:09:26 +0300

    add text-title and theme-button to storybook

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Button from '../components/button'
 import NavigationButton from '../components/navigation-button'
 import Navigation from '../components/navigation'
-import TitleBold from '../components/title-bold'
+import TextTitle from '../components/text-title'
 import * as Icons from '../components/icons'
+import ThemeButton from '../components/theme-button'
+import Stack from '../components/stack'
 
 export default {
   title: 'Buttons',
@@ -11,11 +13,18 @@ export default {
 }
 
 export const Normal = () => <Button>Save</Button>
+export const Theme = () => (
+  <Stack column>
+    <ThemeButton>Tweet</ThemeButton>
+    <ThemeButton fullW>Tweet</ThemeButton>
+    <ThemeButton fullW taller>Tweet Big</ThemeButton>
+  </Stack>
+)
 
 export const NavButton = () => (
   <NavigationButton>
     <Icons.Home />
-    <TitleBold>Home</TitleBold>
+    <TextTitle>Home</TextTitle>
   </NavigationButton>
 )
 
