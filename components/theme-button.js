8ffcdb7f42eb82237f6eb8ab9b6fd4fe62aa0c91
Fatih <fatihcan.social@gmail.com>
2020-06-11 21:08:06 +0300

    add theme button component

---
+++ components/theme-button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './button'
+import styles from './theme-button.module.css'
+import cn from 'classnames'
+
+const ThemeButton = ({ taller = false, className, children, ...props }) => (
+  <Button className={cn(styles.button, taller && styles.taller, className)} {...props}>
+    {children}
+  </Button>
+)
+
+export default ThemeButton
