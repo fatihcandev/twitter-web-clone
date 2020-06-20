d36a8b60bd627024415d1c87a4a285a2cda2f936
Fatih <fatihcan.social@gmail.com>
2020-06-20 22:26:50 +0300

    create home page with layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Button from '../components/button'
+import React from 'react'
+import Layout from '../components/layout'
+import useWindowSize from '../hooks/useWindowSize'
+import CONST from '../constants'
+import Sidebar from '../components/col-sidebar'
+import Main from '../components/col-main'
+import Extra from '../components/col-extra'
 
-const Home = () => (
-  <div>
-    <h1>Welcome to Next.js!</h1>
-    <Button>Merhaba</Button>
-  </div>
-)
+const Home = () => {
+  const windowSize = useWindowSize()
 
+  return (
+    <Layout>
+      <Sidebar flat={windowSize.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
+      <Main>{JSON.stringify(windowSize)}</Main>
+      {windowSize.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
+    </Layout>
+  )
+}
 export default Home
