23d11fab795af313bf99665042675be5fdcd9c3f
Fatih <fatihcan.social@gmail.com>
2020-06-20 22:28:18 +0300

    fix margin problem

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -4,9 +4,12 @@ import styles from './navigation-button.module.css'
 import cn from 'classnames'
 
 const NavigationButton = ({ notify, selected, children, ...props }) => (
-  <Button className={cn(styles.navButton, selected && styles.navButtonSelected)} {...props}>
+  <Button
+    className={cn(styles.navButton, selected && styles.navButtonSelected)}
+    {...props}
+  >
+    {notify > 0 && <span className={styles.notify}> {notify} </span>}
     {children}
-    {notify && <span className={styles.notify}> {notify} </span>}
   </Button>
 )
 
