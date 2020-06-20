1022d697c0f228fe5c843330e56bee306cb15ef6
Fatih <fatihcan.social@gmail.com>
2020-06-20 22:29:16 +0300

    add min-width to keep it round

--- components/theme-button.js
+++ components/theme-button.js
@@ -4,7 +4,10 @@ import styles from './theme-button.module.css'
 import cn from 'classnames'
 
 const ThemeButton = ({ taller = false, className, children, ...props }) => (
-  <Button className={cn(styles.button, taller && styles.taller, className)} {...props}>
+  <Button
+    className={cn(styles.button, taller && styles.taller, className)}
+    {...props}
+  >
     {children}
   </Button>
 )
