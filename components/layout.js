8a0d4cdcd948c32bf0a5b61d71066a7d48666529
Fatih <fatihcan.social@gmail.com>
2020-06-26 17:29:12 +0300

    make constant layout

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,24 @@
 import React from 'react'
+
 import styles from './layout.module.css'
 import cn from 'classnames'
+import useWindowSize from '../hooks/useWindowSize'
 
-const Layout = ({ children }) => (
-  <div className={cn(styles.layout)}>{children}</div>
-)
+import Sidebar from './col-sidebar'
+import CONST from '../constants'
+import Main from './col-main'
+import Extra from './col-extra'
+
+const Layout = ({ children }) => {
+  const windowSize = useWindowSize()
+
+  return (
+    <div className={cn(styles.layout)}>
+      <Sidebar flat={windowSize.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
+      <Main>{children}</Main>
+      {windowSize.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
+    </div>
+  )
+}
 
 export default Layout
