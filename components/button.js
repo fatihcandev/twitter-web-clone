d97b6afffe3f1045113f05b1d027ea7bb3120606
Fatih <fatihcan.social@gmail.com>
2020-06-26 17:31:43 +0300

    render link button or base button according to href prop

--- components/button.js
+++ components/button.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import styles from './button.module.css'
 import cn from 'classnames'
+import Link from 'next/link'
 
-const Button = ({ fullW = false, children, className, ...props }) => (
-  <button
-    type="button"
-    className={cn(styles.button, fullW && styles.fullW, className)}
-    {...props}
-  >
+const LinkButton = ({ href, children, ...props }) => (
+  <Link href={href}>
+    <a {...props}>{children}</a>
+  </Link>
+)
+
+const BaseButton = ({ children, ...props }) => (
+  <button type="button" {...props}>
     {children}
   </button>
 )
+
+const Button = ({ fullW = false, children, className, ...props }) => {
+  const Component = props.href ? LinkButton : BaseButton
+
+  return (
+    <Component
+      className={cn(styles.button, fullW && styles.fullW, className)}
+      {...props}
+    >
+      {children}
+    </Component>
+  )
+}
 
 export default Button
