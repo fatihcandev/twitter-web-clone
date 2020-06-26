77de4c50a596e108c3252f8c4408c20668e078dd
Fatih <fatihcan.social@gmail.com>
2020-06-26 17:33:17 +0300

    add className prop to prevent conflict

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -3,9 +3,21 @@ import Button from './button'
 import styles from './navigation-button.module.css'
 import cn from 'classnames'
 
-const NavigationButton = ({ notify, selected, children, ...props }) => (
+const NavigationButton = ({
+  href,
+  notify,
+  selected,
+  children,
+  className,
+  ...props
+}) => (
   <Button
-    className={cn(styles.navButton, selected && styles.navButtonSelected)}
+    className={cn(
+      styles.navButton,
+      selected && styles.navButtonSelected,
+      className
+    )}
+    href={href}
     {...props}
   >
     {notify > 0 && <span className={styles.notify}> {notify} </span>}
