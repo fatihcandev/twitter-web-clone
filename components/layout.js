306dc1de288084683a35bc99403c79b6540dc45e
Fatih <fatihcan.social@gmail.com>
2020-07-20 14:55:47 +0300

    add bottom navbar, add width conditional render

--- components/layout.js
+++ components/layout.js
@@ -8,14 +8,18 @@ import Sidebar from './col-sidebar'
 import CONST from '../constants'
 import Main from './col-main'
 import Extra from './col-extra'
+import BottomNavbar from './bottom-navbar'
 
 const Layout = ({ children }) => {
   const windowSize = useWindowSize()
 
   return (
     <div className={cn(styles.layout)}>
-      <Sidebar flat={windowSize.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
+      {windowSize.width >= CONST.MOBILE_SIZE && (
+        <Sidebar flat={windowSize.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
+      )}
       <Main>{children}</Main>
+      {windowSize.width < CONST.MOBILE_SIZE && <BottomNavbar />}
       {windowSize.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
     </div>
   )
