db10941c4afcc5ef73c2c5451eab686faa4980ab
Fatih <fatihcan.social@gmail.com>
2020-07-20 17:08:14 +0300

    add default alt text

--- components/avatar.js
+++ components/avatar.js
@@ -4,7 +4,7 @@ import cn from 'classnames'
 
 const Avatar = ({
   src = 'https://pbs.twimg.com/profile_images/1204050560254717957/ILe194Q1_400x400.jpg',
-  alt,
+  alt = 'Fatih Can',
   size = 47
 }) => (
   <div className={cn([styles.avatar])} style={{ width: size, height: size }}>
